01787de96fab038f2e2ff6e54d7612bf56e979f4
azix-khan <azixkhan.55@gmail.com>
2024-08-12 00:05:50 +0500

    fixing bugs and error

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.top]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.top]);
     return MaterialApp(
       title: 'Fruity Lips',
       debugShowCheckedModeBanner: false,
